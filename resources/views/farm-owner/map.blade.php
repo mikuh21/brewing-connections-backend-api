e5c2d40fe3594688067be4ec3fe462624beaffa7
mikuh21 <mikamsanchez21@gmail.com>
2026-09-19 20:31:27 +0800

    optimize production frontend asset loading

--- resources/views/farm-owner/map.blade.php
+++ resources/views/farm-owner/map.blade.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
           integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
           crossorigin=""/>
-    @vite('resources/css/app.css')
 @endpush
 
 @section('content')

e15be51d005f0ee6a330acf3b07cb3b651445c5a
mikuh21 <mikamsanchez21@gmail.com>
2026-09-25 14:18:59 +0800

    fix Farm Owner Alpine initialization

--- resources/views/farm-owner/layouts/app.blade.php
+++ resources/views/farm-owner/layouts/app.blade.php
@@ -18,8 +18,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 
     {{-- Alpine.js --}}
-    <script src="//unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" 
-            defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 
     <style>
         body { 

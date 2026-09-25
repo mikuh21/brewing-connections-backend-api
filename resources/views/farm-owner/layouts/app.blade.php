f6c32dd0d1b9338f8a957712ca4b6116c8e705cd
mikuh21 <mikamsanchez21@gmail.com>
2026-09-25 14:03:05 +0800

    restore original Farm Owner layout styling

--- resources/views/farm-owner/layouts/app.blade.php
+++ resources/views/farm-owner/layouts/app.blade.php
@@ -14,8 +14,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,600&family=Poppins:wght@300;400;500;600;700&display=swap" 
           rel="stylesheet">
 
-    {{-- Production frontend bundle (Tailwind + shared app CSS) --}}
-    @vite('resources/css/app.css')
+    {{-- Tailwind CDN --}}
+    <script src="https://cdn.tailwindcss.com"></script>
 
     {{-- Alpine.js --}}
     <script src="//unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" 
